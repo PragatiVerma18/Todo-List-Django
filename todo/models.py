6a80b68331ea18c06956f8a4a63e34835d2a9d3f
pragati <itispragativerma@gmail.com>
2019-10-29 02:14:17 +0530

    made migartions

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+  text = models.TextField()
+  date_posted = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return 'Todo #{}'.format(self.id)
+ 
+  class Meta:
+    verbose_name_plural = 'Todos'
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Todo
 
 # Create your views here.
 def index(request):
-  return render(request, "todo/index.html")
+  todos = Todo.objects.order_by("-date_posted")
+  context = {'todos': todos}
+  return render(request, "todo/index.html",context)
 
 def add(request):
   return render(request, "todo/add.html") 
